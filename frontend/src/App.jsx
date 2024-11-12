556c6f76441aee9dd1cda5d7889ad852f69761c5
mahesheninrac <mahesh.solanke@eninrac.com>
2024-11-12 16:51:27 +0530

    added h1 tag with title in app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div>
-    
-  </div>;
+  return (
+    <div className="app">
+      <h1>Hello from react.js</h1>
+    </div>
+  );
 }
 
 export default App;
